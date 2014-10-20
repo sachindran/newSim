8a57464ca6ffcc0b819f034a54aff0f2bff2935c
Sachindran <sachindran1991@gmail.com>
2014-10-20 16:16:56 -0500

    First commit

---
+++ newSim/SimApplet.java
@@ -0,0 +1,10 @@
+package newSim;
+import javax.swing.JApplet;
+
+public class SimApplet extends JApplet  {
+   public SimApplet() {
+   }
+   public void start(){
+      Simulator.main(new String[]  {"Applet"});
+   }
+}
